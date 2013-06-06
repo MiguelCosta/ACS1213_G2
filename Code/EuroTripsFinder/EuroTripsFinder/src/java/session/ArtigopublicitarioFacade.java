8253749a73bc56cf994d858b6f3fb2aec1541297
miltonnunes52 <milton.nunes52@gmail.com>
2013-06-06 14:19:22 +0100

    publicidade por concluir

--- Code/EuroTripsFinder/EuroTripsFinder/src/java/session/ArtigopublicitarioFacade.java
+++ Code/EuroTripsFinder/EuroTripsFinder/src/java/session/ArtigopublicitarioFacade.java
@@ -5,9 +5,12 @@
 package session;
 
 import entity.Artigopublicitario;
+import java.util.List;
+import java.util.Random;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -25,6 +28,16 @@ public class ArtigopublicitarioFacade extends AbstractFacade<Artigopublicitario>
 
     public ArtigopublicitarioFacade() {
         super(Artigopublicitario.class);
+    }
+    
+    public Artigopublicitario ArtigoRandom(String datafim) {
+        //pode nao ser assim
+        Query q = em.createQuery("SELECT a.* FROM ArtigoPublicitario a, Contrato c WHERE a.Contratoid = c.id and c.datafim :datafim");
+        q.setParameter("datafim", datafim);
+        List<Artigopublicitario> artigos = q.getResultList();
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(artigos.size());
+        return artigos.get(randomInt);
     }
     
 }
