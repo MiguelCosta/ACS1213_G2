efcf12a0f29b7d7a9b1999e952f4475fa9c2473d
Miguel Costa <miguelpintodacosta@gmail.com>
2013-06-25 21:16:15 +0100

    Querie para pesquisa da rota adicionada

--- Code/EuroTripsFinderDB/GTFS/sql/queries.sql
+++ Code/EuroTripsFinderDB/GTFS/sql/queries.sql
@@ -1,16 +1,29 @@
 
+
 SELECT * FROM tempoparagem WHERE transporteviagemid IN (
 
-SELECT DISTINCT transporteviagemid FROM tempoparagem 
-WHERE transporteviagemid 
-	IN 
-	   (SELECT DISTINCT transporteviagemid FROM tempoparagem WHERE localparagemid=:stop_id_origem) 
-AND transporteviagemid 
-	IN 
-       (SELECT DISTINCT transporteviagemid FROM tempoparagem WHERE localparagemid=:stop_id_destino)
+SELECT 
+transporteviagem.transporteviagemid
+
+FROM transporteviagem,calendario,rota
+
+WHERE transporteviagemid IN (
+
+		SELECT DISTINCT transporteviagemid FROM tempoparagem
+		WHERE transporteviagemid 
+			IN 
+			   (SELECT DISTINCT transporteviagemid FROM tempoparagem WHERE localparagemid=7848) 
+		AND transporteviagemid 
+			IN 
+			   (SELECT DISTINCT transporteviagemid FROM tempoparagem WHERE localparagemid=7849)
+
+	)
+AND transporteviagem.servicoid = calendario.servicoid
+AND transporteviagem.Rotaid = rota.id
+AND calendario.datainicio >= '2012-01-01'
+AND calendario.datafim <= '2012-12-31'
+AND calendario.quarta = 1
 
 )
-
-
-
+AND localparagemid=7848 OR localparagemid=7849
 
